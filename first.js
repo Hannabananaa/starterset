41d793a1b28aeca16a7ca0f6cdbe2f1dee20a400
Hannabananaa <154515877+Hannabananaa@users.noreply.github.com>
2023-12-21 11:04:53 +0000

    Hannas commit

--- first.js
+++ first.js
@@ -4,33 +4,35 @@ function sayHello(name) {
 
 sayHello('Weihnachtsmann')
 // say hello to a friend
+sayHello('Judith')
 
 
 // initialize the best name ever =  give it the initial value and greet them
-// const bestNameEver = ;
+ const bestNameEver ='Alex'
+sayHello(bestNameEver)
 
 
-let bestCollegueEver;
+let bestCollegueEver='Mihali'
 // who is the best colleague? greet them
-
-
-
+sayHello(bestCollegueEver)
+bestCollegueEver='Rhena'
+sayHello(bestCollegueEver)
 // but maybe your favorite changes?
 // bestCollegueEver = ;
 
 // let's greet all of our colleagues
 const allColleagues = ["Patrik", "Suresh", "Robin", "Daniel", "Corinna", "Tito", "Nico"]
-sayHello(allColleagues)
+// sayHello(allColleagues)
 
 // only greet the third colleague
 
 // greet colleagues in alphabetical order
-sayHello(allColleagues.sort())
+//sayHello(allColleagues.sort())
 
 // greet only colleagues which have an 'i' in their name
 for (let name of allColleagues) {
     if (name.includes('i')) {
-        sayHello(name)
+        //sayHello(name)
     }
 }
 
